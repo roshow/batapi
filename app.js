d569d352eb7bd9a482b42aaf2f12f76a6653dda5
roshow <rolando@roshow.net>
2014-04-11 23:15:45 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict';
+var restify = require('restify'),
+    handler = require('./handler'),
+    server = restify.createServer({ name: 'thinkbatman' }),
+    port = process.env.PORT || 7777,
+    routes;
+console.log(process.env.PORT);
+routes = [
+    {
+        path:/^\/(?:bat-|)thought(?:\/(.*)|)$/,
+        action: handler.thought.get,
+        method: 'get'
+    },
+    {
+        path:/^\/(?:bat-|)thought(?:\/|)$/,
+        action: handler.thought.get,
+        method: 'put'
+    }
+];
+    
+function startServer(){
+    server
+        .use(restify.queryParser())
+        .use(restify.fullResponse())
+        .use(restify.bodyParser());
+
+    routes.forEach(function(route){
+        server[route.method](route.path, route.action);
+    });
+
+    server.listen(port, function(){
+        console.log('%s listening at %s', server.name, server.url);
+    });
+    // return server;
+}
+
+handler.connectDb().then(startServer);
+// module.exports = handler.connectDb().then(startServer);
+
